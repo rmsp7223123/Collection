74c954e1c3e21198dd5185f47ae7e3af32b2d059
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-11-27 21:15:08 +0900

    ++++

--- app/src/main/java/com/example/collection/JavaActivity.java
+++ app/src/main/java/com/example/collection/JavaActivity.java
@@ -4,13 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class JavaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java);
-        
+
 
     }
 }
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        HashMap<String, Integer> resultMap = new HashMap<>();
+
+        for (int i = 0; i < name.length; i++) {
+            resultMap.put(name[i], yearning[i]);
+        };
+
+        for (int i = 0; i < photo.length; i++) {
+            String[] person = photo[i];
+            int score = 0;
+            for (int j = 0; j < person.length; j++) {
+                String personName = person[j];
+                if(resultMap.containsKey(personName)) {
+                    score += resultMap.get(personName);
+                };
+            };
+            answer[i] = score;
+        };
+
+        return answer;
+    };
+};
